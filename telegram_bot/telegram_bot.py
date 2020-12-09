e571ea1016ad94ae5b8535c99aef506126d7077d
parksanguk <tkddnr7762@gmail.com>
2020-12-09 11:14:30 +0900

    201209_update_1

--- telegram_bot/telegram_bot.py
+++ telegram_bot/telegram_bot.py
@@ -14,16 +14,16 @@ def c19_bot():
     url = "http://ncov.mohw.go.kr/bdBoardList_Real.do?brdId=1&brdGubun=13&ncvContSeq=&contSeq=&board_id=&gubun="
     soup = create_soup(url)
 
-    results = []
+    
     c19_infos = soup.find("tbody").find_all("tr")
     for c19_info in c19_infos:
         area = c19_info.find("th").text
         if area == "합계" or area == "대전" or area == "충북":
-            c19_status = c19_info.find_all("td", limit=3)
-            for c19 in c19_status:
-                results.append(c19.text)
-    
-    return results
+            c19_sum = c19_info.find("td", attrs={"headers": "status_level l_type1"}).text
+            c19_1 = c19_info.find("td", attrs={"headers": "status_level l_type2"}).text
+            c19_2= c19_info.find("td", attrs={"headers": "status_level l_type3"}).text
+            result = result + f"---------- {c19_sum} ----------\n국내발생 : {c19_1}\n 해외유입 : {c19_2}"
+    return result
 
 
 if __name__ == "__main__":
